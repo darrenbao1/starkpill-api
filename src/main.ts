9e78d4b9b8c84331ee255aa67b334e37b7dc99de
dbao <dbao@seraphlabs.io>
2023-08-30 16:47:09 +0800

    added CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,16 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({
+    origin: [
+      'http://localhost:3001', // remove when in production.
+      'https://starkpill.me',
+      'https://www.starkpill.me',
+      'https://starkpill.clinic',
+      'https://www.starkpill.clinic',
+      'https://starkpill.vercel.app',
+    ],
+  });
   await app.listen(app.get(ConfigService).get('PORT'));
 }
 bootstrap();
