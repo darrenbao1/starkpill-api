27cf6c8d747106ea39d07f3b5f9682782adfc902
dbao <dbao@seraphlabs.io>
2023-09-25 17:24:45 +0800

    removing localhost domain

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors({
     origin: [
-      'http://localhost:3000', // remove when in production.
       'https://starkpill.me',
       'https://www.starkpill.me',
       'https://starkpill.clinic',
