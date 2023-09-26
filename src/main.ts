3437108057aa588430f146b73e3b9ddddd05c477
dbao <dbao@seraphlabs.io>
2023-09-26 16:51:32 +0800

    add localhost

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors({
     origin: [
+      'http://localhost:3000',
       'https://starkpill.me',
       'https://www.starkpill.me',
       'https://starkpill.clinic',
